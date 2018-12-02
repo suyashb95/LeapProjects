2e50faa0e67a815abf9fe3a12089d83d4bdde15b
Suyash458 <suyash.behera458@gmail.com>
2018-12-02 13:01:46 +0530

    add lines to FingerPlot

--- Python/FingerPlot.py
+++ Python/FingerPlot.py
@@ -1,5 +1,6 @@
 import Leap
 import sys
+import mpl_toolkits.mplot3d as plt3d
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from matplotlib import animation
@@ -11,11 +12,20 @@ controller.set_policy_flags(Leap.Controller.POLICY_BACKGROUND_FRAMES)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d', xlim=(-300, 400), ylim=(-200, 400), zlim=(-300, 300))
 ax.view_init(elev=10., azim=250)
-
-points = np.zeros((3, 6))
 colors = np.array([100, 100, 100, 100, 100, 500])
 
-patches = ax.scatter(points[0], points[1], points[2], c='red', s=[20, 20, 20, 20, 20, 50])
+points = np.zeros((3, 6))
+
+lines = []
+
+palm_x, palm_y, palm_z = points.T[5]
+for (x, y, z) in points[:, :5].T:
+	line = plt3d.art3d.Line3D([x, palm_x], [y, palm_y], [z, palm_z])
+	lines.append(line)
+	ax.add_line(line)
+
+patches = ax.scatter(points[0], points[1], points[2], s=[30, 30, 30, 30, 30, 100], alpha=1)
+patches.set_array(colors)
 
 def get_points():
 	frame = controller.frame()
@@ -30,22 +40,30 @@ def get_points():
 	Z.append(hand.palm_position.z)
 	return np.array([X, Z, Y])
 
-def plot_points():
-	points = get_points()
+def plot_lines(points):
+	palm_x, palm_y, palm_z = points.T[5]
+	for (i, line) in enumerate(lines):
+		line.set_data([points[0, i], palm_x], [points[1, i], palm_y])
+		line.set_3d_properties([points[2, i], palm_z])
+
+def plot_points(points):
 	patches.set_offsets(points[:2].T)
 	patches.set_3d_properties(points[2], zdir='z')
-	patches.set_array(colors)
 
 def init():
-	plot_points()
+	points = get_points()
+	plot_points(points)
+	plot_lines(points)
 	return patches,
 
 def animate(i):
-	plot_points()
+	points = get_points()
+	plot_points(points)
+	plot_lines(points)
 	return patches,
 
 def main():
-	anim = animation.FuncAnimation(fig, animate, init_func=init, blit=False, frames=360, interval=20)
+	anim = animation.FuncAnimation(fig, animate, init_func=init, blit=False, interval=2)
 	try:
 		plt.show()
 	except KeyboardInterrupt:
