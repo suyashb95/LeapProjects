780e27985d91a051a739caf5f55cae7ea861f0f6
Suyash458 <suyash.behera458@gmail.com>
2019-03-26 19:42:53 +0530

    improve calibration, remove unnecessary points from visualisation

--- Python/ImageProcessing/calibrate_camera.py
+++ Python/ImageProcessing/calibrate_camera.py
@@ -5,58 +5,20 @@ import Leap
 
 import numpy as np
 
-DESTINATION_HEIGHT = 240
-DESTINATION_WIDTH = 640
+from camera_constants import C1, C2, D1, D2, R, T, E, F, Q, DESTINATION_HEIGHT, DESTINATION_WIDTH
 
-board_size = (3, 3)
+board_size = (5, 4)
 
+square_size = 31
 num_boards = board_size[0] * board_size[1]
 
 destination = np.empty((DESTINATION_HEIGHT, DESTINATION_WIDTH), dtype=np.ubyte)
 
-obj = np.array([np.array((i / board_size[0], i % board_size[0], 0.0), dtype=np.float32) for i in range(num_boards)])
+obj = np.array([np.array(((i / board_size[0]) * square_size, (i % board_size[0]) * square_size, 0.0), dtype=np.float32) for i in range(num_boards)])
 object_points, image_points1, image_points2 = [], [], []
 
-stereocalib_criteria = (cv2.TERM_CRITERIA_MAX_ITER + cv2.TERM_CRITERIA_EPS, 30, 1e-6)
-stereocalib_flags = cv2.CALIB_FIX_INTRINSIC | cv2.CALIB_FIX_FOCAL_LENGTH | cv2.CALIB_FIX_PRINCIPAL_POINT
-
-C1 = np.array([
-    [134.6, 0, 327.8],
-    [0, 67.4, 120.3],
-    [0, 0, 1]
-])
-
-C2 = np.array([
-    [134.3, 0, 323.2],
-    [0, 67.3, 123.4],
-    [0, 0, 1]
-])
-
-D1 = np.array([[-0.396571, -0.80373126, 0.14070022, -0.24970651, 0.67173672]])
-D2 = np.array([[ 0.155679, -1.09981795, 0.19912512, -0.00359546, 0.97444669]])
-R = np.array([
-    [0.99907293, -0.03373247, -0.02674706],
-    [0.02385673, 0.95100281, -0.30826047],
-    [0.03583491, 0.30733659, 0.95092591]
-])
-T = np.array([[1.96287273], [1.64604608], [-1.13382269]])
-E = np.array([
-    [0.08603522, 1.58415876, 1.21575515],
-    [-1.20311093, -0.56501597, -1.83622011],
-    [-1.59769235, 1.92222268, -0.56104918]
-])
-F = np.array([
-    [1.11375187e-05, 4.09539717e-04, -3.17347325e-02],
-    [-3.10798405e-04, -2.91486869e-04, 7.30982328e-02],
-    [6.97613364e-03, -2.96551658e-02, 1.00000000e+00]
-])
-
-Q = np.array([
-    [1., 0., 0., -280.7162652 ],
-    [0., 1., 0. , -118.74788302],
-    [0., 0., 0., 67.35],
-    [0., 0., 0.35696326, -0.]
-])
+stereocalib_criteria = (cv2.TERM_CRITERIA_MAX_ITER + cv2.TERM_CRITERIA_EPS, 500, 0.00000000001)
+stereocalib_flags = cv2.CALIB_USE_INTRINSIC_GUESS | cv2.CALIB_FIX_FOCAL_LENGTH
 
 def initDistortionMap(image):
     distortion_length = image.distortion_width * image.distortion_height
@@ -70,11 +32,11 @@ def initDistortionMap(image):
     ymap = np.reshape(ymap, (image.distortion_height, image.distortion_width // 2))
 
     #resize the distortion map to equal desired destination image size
-    expanded_xmap = cv2.resize(xmap, (DESTINATION_WIDTH, DESTINATION_HEIGHT), 0, 0, cv2.INTER_LINEAR)
-    expanded_ymap = cv2.resize(ymap, (DESTINATION_WIDTH, DESTINATION_HEIGHT), 0, 0, cv2.INTER_LINEAR)
+    expanded_xmap = cv2.resize(xmap, (DESTINATION_WIDTH, DESTINATION_HEIGHT), 0, 0, cv2.INTER_CUBIC)
+    expanded_ymap = cv2.resize(ymap, (DESTINATION_WIDTH, DESTINATION_HEIGHT), 0, 0, cv2.INTER_CUBIC)
     return expanded_xmap, expanded_ymap
 
-def interpolate(image, xmap, ymap):
+def get_image_as_array(image):
     #wrap image data in numpy array
     i_address = int(image.data_pointer)
     ctype_array_def = ctypes.c_ubyte * image.height * image.width
@@ -82,13 +44,14 @@ def interpolate(image, xmap, ymap):
     as_ctype_array = ctype_array_def.from_address(i_address)
     # as numpy array
     as_numpy_array = np.ctypeslib.as_array(as_ctype_array)
-    img = np.reshape(as_numpy_array, (image.height, image.width))
+    return np.reshape(as_numpy_array, (image.height, image.width))
 
-    #remap image to destination
-    return cv2.remap(img, xmap, ymap, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+def interpolate(img, xmap, ymap):
+    return cv2.remap(img, xmap, ymap, interpolation=cv2.INTER_CUBIC, borderMode=cv2.BORDER_CONSTANT)
 
 def process(controller):
     map_initialized = False
+    num_images = 0
     num_samples = 0
     while True:
         frame = controller.frame()
@@ -98,10 +61,10 @@ def process(controller):
                 left_x_map, left_y_map = initDistortionMap(frame.images[0])
                 right_x_map, right_y_map = initDistortionMap(frame.images[1])
                 map_initialized = True
-            undistorted_left = interpolate(images[0], left_x_map, left_y_map)
-            undistorted_right = interpolate(images[1], left_x_map, left_y_map)
-            left_bw = cv2.adaptiveThreshold(undistorted_left, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 21, 2)
-            right_bw = cv2.adaptiveThreshold(undistorted_right, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 21, 2)
+            undistorted_left = interpolate(get_image_as_array(images[0]), left_x_map, left_y_map)
+            undistorted_right = interpolate(get_image_as_array(images[1]), left_x_map, left_y_map)
+            left_bw = cv2.adaptiveThreshold(undistorted_left, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 13, 3)
+            right_bw = cv2.adaptiveThreshold(undistorted_right, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 13, 3)
             corners_left = cv2.findChessboardCorners(left_bw, board_size, flags=cv2.CALIB_CB_FILTER_QUADS)
             corners_right = cv2.findChessboardCorners(right_bw, board_size, flags=cv2.CALIB_CB_FILTER_QUADS)
             if corners_left[0] and corners_right[0]:
@@ -113,17 +76,29 @@ def process(controller):
                 image_points2.append(corners_right[1])
                 object_points.append(obj)
                 num_samples += 1
+                print(num_samples)
                 if num_samples > num_boards:
                     break
             cv2.imshow('left', undistorted_left)
             cv2.imshow('right', undistorted_right)
+            cv2.imshow('left_bw', left_bw)
+            cv2.imshow('right_bw', right_bw)
             if cv2.waitKey(1) & 0xFF == ord('q'):
-                #cv2.imwrite('left.jpg', undistorted_left)
-                #cv2.imwrite('right.jpg', undistorted_right)
+                cv2.imwrite(f'left{num_images}.jpg', left_bw)
+                cv2.imwrite(f'right{num_images}.jpg', right_bw)
+                num_images += 1
                 break
-    #retval,cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, R, T, E, F = cv2.stereoCalibrate(object_points, image_points1, image_points2, C1, D1, C2, D2, imageSize=(640, 240), flags=stereocalib_flags, criteria=stereocalib_criteria)
+
+    retval,cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, R, T, E, F = cv2.stereoCalibrate(
+        object_points, image_points1, image_points2, C1, D1, C2, D2, imageSize=(640, 240), flags=stereocalib_flags, criteria=stereocalib_criteria
+    )
+
+    print("params")
+    print(retval,cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, R, T, E, F)
+
+    print("reprojection matrix")
     a = cv2.stereoRectify(C1, D1, C2, D2, (640, 240), R, T)
-    print(a[-1])
+    print(a[-3])
 
 def main():
     controller = Leap.Controller()
--- Python/ImageProcessing/image_correction.py
+++ Python/ImageProcessing/image_correction.py
@@ -21,15 +21,15 @@ w.addItem(g)
 
 destination = np.empty((DESTINATION_HEIGHT, DESTINATION_WIDTH), dtype=np.ubyte)
 
-stereo_matcher = cv2.StereoBM_create(numDisparities=16*8, blockSize=11)
-stereo_matcher.setMinDisparity(-80)
-stereo_matcher.setUniquenessRatio(50)
+stereo_matcher = cv2.StereoBM_create(numDisparities=16*8, blockSize=7)
+stereo_matcher.setMinDisparity(-100)
+stereo_matcher.setUniquenessRatio(10)
 
 stereo_matcher.setPreFilterType(cv2.STEREO_BM_PREFILTER_NORMALIZED_RESPONSE)
-stereo_matcher.setPreFilterSize(7)
+stereo_matcher.setPreFilterSize(5)
 stereo_matcher.setPreFilterCap(50)
 
-stereo_matcher.setSpeckleWindowSize(10)
+stereo_matcher.setSpeckleWindowSize(50)
 stereo_matcher.setSpeckleRange(10)
 
 wls_filter = cv2.ximgproc.createDisparityWLSFilter(stereo_matcher)
@@ -67,11 +67,12 @@ def interpolate(image, xmap, ymap):
     # as numpy array
     as_numpy_array = np.ctypeslib.as_array(as_ctype_array)
     img = np.reshape(as_numpy_array, (image.height, image.width))
-
     #remap image to destination
-    return cv2.remap(img, xmap, ymap, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+    return cv2.remap(img, xmap, ymap, interpolation=cv2.INTER_CUBIC, borderMode=cv2.BORDER_CONSTANT)
 
-scatterplot_items = None
+scatterplot_items = gl.GLScatterPlotItem(pos=np.array([]), color=(1,1,1,.3), size=0.05, pxMode=False)
+scatterplot_items.rotate(180, 1, 0, 0)
+w.addItem(scatterplot_items)
 
 def process():
     global map_initialized, scatterplot_items
@@ -80,20 +81,12 @@ def process():
     if images[0].is_valid and images[1].is_valid:
         undistorted_left = interpolate(images[0], left_x_map, left_y_map)
         undistorted_right = interpolate(images[1], right_x_map, right_y_map)
-        # cv2.imshow('left', undistorted_left)
-        # cv2.imshow('right', undistorted_right)
         dispL = np.int16(stereo_matcher.compute(undistorted_left, undistorted_right))
         dispR = np.int16(right_matcher.compute(undistorted_right, undistorted_left))
         filteredDisparity = wls_filter.filter(dispL, undistorted_left, None, dispR)
-        depth = cv2.normalize(filteredDisparity, None, beta=0, alpha=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-        points = cv2.reprojectImageTo3D(filteredDisparity, Q)
-        #cv2.imshow('depth', points)
-        if not scatterplot_items:
-            scatterplot_items = gl.GLScatterPlotItem(pos=points.reshape(640 * 240, 3), color=(1,1,1,.3), size=0.005, pxMode=False)
-            scatterplot_items.rotate(180, 1, 0, 0)
-            w.addItem(scatterplot_items)
-        else:
-            scatterplot_items.setData(pos=points.reshape(640 * 240, 3))
+        points = cv2.reprojectImageTo3D(filteredDisparity, Q, handleMissingValues=True).reshape(640 * 240, 3)
+        points = points[points[:,2] < 0]
+        scatterplot_items.setData(pos=points)
 
 t = QtCore.QTimer()
 t.timeout.connect(process)
